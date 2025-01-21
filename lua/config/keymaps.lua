9c2f8352072803a273dae293521d9f645ccbe222
linjie <meetlinjie@gmail.com>
2025-01-21 15:21:25 +0800

    blink

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,5 +4,3 @@ map.set("n", "H", "^", { noremap = true })
 map.set("n", "L", "$", { noremap = true })
 map.set("n", "<leader>w", "<cmd>w<cr>", { noremap = true, desc = "save file" })
 map.set("n", "<leader>\\", "<cmd>vsplit<cr>", { noremap = true, desc = "split the window vertically" })
-
-map.set("n", ";", ":", { desc = "CMD enter command mode" })
--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -2,14 +2,14 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
+    'saghen/blink.cmp',
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 		{ "folke/neodev.nvim", opts = {} },
 	},
 	config = function()
 		local lspconfig = require("lspconfig")
 		local mason_lspconfig = require("mason-lspconfig")
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		-- local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 		vim.lsp.inlay_hint.enable()
 		vim.api.nvim_create_autocmd("LspAttach", {
@@ -58,7 +58,8 @@ return {
 		})
 
 		-- used to enable autocompletion (assign to every lsp server config)
-		local capabilities = cmp_nvim_lsp.default_capabilities()
+		-- local capabilities = cmp_nvim_lsp.default_capabilities()
+    local capabilities = require('blink.cmp').get_lsp_capabilities()
 
 		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
 		for type, icon in pairs(signs) do
